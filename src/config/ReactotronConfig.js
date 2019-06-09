0b8d4fea9ab7c23872c6eb9d195adc3a919c45b2
condini-mastheus <matheus.condini@gmail.com>
2019-06-09 10:28:02 -0300

    Remove redux-saga do template e adiciona configurações de estilo da aplicação

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,9 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
-import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: '10.0.3.2' })
     .use(reactotronRedux())
-    .use(sagaPlugin())
     .connect();
 
   tron.clear();
---
+++ src/styles/colors.js
@@ -0,0 +1,15 @@
+export default {
+  // defaults
+  white: '#fff',
+  lighter: '#eee',
+  light: '#ddd',
+  regular: '#999',
+  dark: '#666',
+  darker: '#333',
+  black: '#000',
+  transparent: 'transparent',
+  blackTransparent: opacity => `rgba(0, 0, 0, ${opacity})`,
+  whiteTransparent: opacity => `rgba(255, 255, 255, ${opacity})`,
+  primary: '#e5556e',
+  secundary: '#23202c',
+};
